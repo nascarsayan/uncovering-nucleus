5faf471a360b8e22d4a8b4d5084518ed0f1817c9
Lokesh Mohanty <lokesh1197@gmail.com>
2019-03-02 23:57:13 +0530

    Improve code readability

--- init.py
+++ init.py
@@ -1,24 +1,31 @@
-import networkx as nx
-import csv
-import matplotlib.pyplot as plt
-from sys import argv
-import json
+# =================================================== 
+# ===================== Import ====================== 
+# =================================================== 
 import os
+from sys import argv
 import re
+import csv
+import json
 from collections import Counter
+import networkx as nx
+import matplotlib.pyplot as plt
 import plotly.offline as py
 import plotly.graph_objs as go
 
-efile = './dummyDataset/edges.csv'
-plotlykey = 'LuM1q5YsmH2ItxMWeE8I'
+EDGES_FILE = './dummyDataset/edges.csv'
 if len(argv) > 1:
-  efile = argv[1]
-corenum = None
-if len(argv) > 2:
-  corenum = int(argv[2])
-dataset = os.path.splitext(os.path.basename(efile))[0]
-outputDir = './out/%s' % (efile)
+  EDGES_FILE = argv[1]
+OUTPUT_DIR = './out/%s' % (EDGES_FILE)
+plotlykey = 'LuM1q5YsmH2ItxMWeE8I'
 
+core_num = None
+if len(argv) > 2:
+  core_num = int(argv[2])
+DATASET = os.path.splitext(os.path.basename(EDGES_FILE))[0]
+
+# =================================================== 
+# ==================== Functions ==================== 
+# =================================================== 
 
 def dumpData(data, fname):
   with open(fname, 'w') as fp:
@@ -37,12 +44,12 @@ def readEdges(fname):
       if ',' in line:
         delim = ','
       rows.append((list(
-          map(lambda x: int(x.strip()), re.split(delim, line.strip()))))[:2])
+        map(lambda x: int(x.strip()), re.split(delim, line.strip()))))[:2])
       for line in txtfile:
         try:
           rows.append((list(
-              map(lambda x: int(x.strip()), re.split(delim,
-                                                     line.strip()))))[:2])
+            map(lambda x: int(x.strip()), re.split(delim,
+              line.strip()))))[:2])
         except Exception as _:
           pass
   else:
@@ -52,6 +59,62 @@ def readEdges(fname):
         rows.append(list(map(int, row)))
 
   return rows
+
+
+def getDep(G, cores, OUTPUT_DIR):
+  betas = list(map(lambda x: x / 10, list(range(11))))
+  nodes = G.nodes()
+  edges = G.edges()
+  k_max = max(cores.values())
+  print('k_max = %d' %(k_max))
+  ni = []
+  aggni = []
+  for beta in betas:
+    print('+++ β = %0.1f' % (beta))
+    depBeta = [{v: 0 for v in nodes}]
+    for k in range(1, k_max + 1):
+      depBetaK = {}
+      for v in nodes:
+        depBetaK[v] = depBeta[k - 1][v]
+        if k > cores[v]:
+          continue
+        nbrs = list(
+            filter(lambda u: cores[u] == k, list(nx.all_neighbors(G, v))))
+        depBetaK[v] += len(nbrs) + beta * sum(
+            list(map(lambda u: depBeta[k - 1][u], nbrs)))
+        depBeta.append(depBetaK)
+    # print(list(map(lambda x: list(x.values()) , depBeta)))
+    dumpData(list(map(lambda x: list(x.values()) , depBeta)), '%s/dep/%d.json' % (OUTPUT_DIR, beta * 10))
+    sumDep = sum(depBeta[-1].values())
+    vk_1 = nodes
+    ek_1 = edges
+    nib = [0]
+    for k in range(1, k_max):
+      vk = list(filter(lambda u: cores[u] > k, vk_1))
+      ek = list(filter(lambda e: cores[e[0]] > k and cores[e[1]] > k, ek_1))
+      nibk = (1 / len(vk_1)) * (len(ek) / (len(vk) * (len(vk) - 1))) * (
+          sum(list(map(lambda i: depBeta[-1][i], vk))) / sumDep)
+      nib.append(nibk)
+      vk_1 = vk
+      ek_1 = ek
+    nib += [0]
+    dumpData(nib, '%s/ni/%d.json' % (OUTPUT_DIR, beta * 10))
+    mni = nib.index(max(nib))
+    print('Nuclear Index = ', mni)
+    ni.append(nib)
+    aggni.append(mni)
+    # print('# nodes in the core with max NI = %d; # nodes in the core, with max k = %d' % (len(list(filter(lambda x: cores[x] >= mni, nodes))), len(list(filter(lambda x: cores[x] == k_max, nodes)))))
+  # lineGraphs(ni, betas, '%s NuclearIndex' % (DATASET),
+  #            '%s/ni/graph.png' % (OUTPUT_DIR))
+  plotlyGraphs(ni, betas, '%s NuclearIndex' % (DATASET),
+      '%s/ni/graph.html' % (OUTPUT_DIR))
+  cnt = Counter(aggni)
+  kc = (cnt.most_common(1))[0][0]
+  chosenBs = list(
+      map(lambda x: betas[x], filter(lambda y: aggni[y] == kc, range(10))))
+  print('@@@ k_C = %d, β chosen = %r' % (kc, chosenBs[len(chosenBs) // 2]))
+  nucleus = nx.k_core(G, kc)
+  print('N(GC) = %d, E(GC) = %d' %(nucleus.number_of_nodes(), nucleus.number_of_edges()))
 
 
 def drawGraph(G, figname='graph.png'):
@@ -76,106 +139,67 @@ def lineGraphs(y, label, title='line-graph', filename='line.png', clf=True):
 
 def plotlyGraphs(y, label, title='line-graph', filename='line.png', clf=True):
   layout = go.Layout(
-    title=title,
-    xaxis=dict(
+      title=title,
+      xaxis=dict(
         title='G_k (k-core)s',
-    ),
-    yaxis=dict(
+        ),
+      yaxis=dict(
         title='NI (G_k, dep(i, β))',
-    )
-)
+        )
+      )
   x = list(range(len(y[0])))
   trace = list(map(lambda i: go.Scatter(x=x, y=y[i], name=label[i]), list(range(len(y)))))
   fig = go.Figure(data=trace, layout=layout)
   py.plot(fig, filename=filename)
 
 
-def getDep(G, cores, outputDir):
-  betas = list(map(lambda x: x / 10, list(range(11))))
-  nodes = G.nodes()
-  edges = G.edges()
-  k_max = max(cores.values())
-  print('k_max = %d' %(k_max))
-  ni = []
-  aggni = []
-  for beta in betas:
-    print('+++ β = %0.1f' % (beta))
-    depBeta = [{v: 0 for v in nodes}]
-    for k in range(1, k_max + 1):
-      depBetaK = {}
-      for v in nodes:
-        depBetaK[v] = depBeta[k - 1][v]
-        if k > cores[v]:
-          continue
-        nbrs = list(
-            filter(lambda u: cores[u] == k, list(nx.all_neighbors(G, v))))
-        depBetaK[v] += len(nbrs) + beta * sum(
-            list(map(lambda u: depBeta[k - 1][u], nbrs)))
-      depBeta.append(depBetaK)
-    # print(list(map(lambda x: list(x.values()) , depBeta)))
-    dumpData(list(map(lambda x: list(x.values()) , depBeta)), '%s/dep/%d.json' % (outputDir, beta * 10))
-    sumDep = sum(depBeta[-1].values())
-    vk_1 = nodes
-    ek_1 = edges
-    nib = [0]
-    for k in range(1, k_max):
-      vk = list(filter(lambda u: cores[u] > k, vk_1))
-      ek = list(filter(lambda e: cores[e[0]] > k and cores[e[1]] > k, ek_1))
-      nibk = (1 / len(vk_1)) * (len(ek) / (len(vk) * (len(vk) - 1))) * (
-          sum(list(map(lambda i: depBeta[-1][i], vk))) / sumDep)
-      nib.append(nibk)
-      vk_1 = vk
-      ek_1 = ek
-    nib += [0]
-    dumpData(nib, '%s/ni/%d.json' % (outputDir, beta * 10))
-    mni = nib.index(max(nib))
-    print('Nuclear Index = ', mni)
-    ni.append(nib)
-    aggni.append(mni)
-    # print('# nodes in the core with max NI = %d; # nodes in the core, with max k = %d' % (len(list(filter(lambda x: cores[x] >= mni, nodes))), len(list(filter(lambda x: cores[x] == k_max, nodes)))))
-  # lineGraphs(ni, betas, '%s NuclearIndex' % (dataset),
-  #            '%s/ni/graph.png' % (outputDir))
-  plotlyGraphs(ni, betas, '%s NuclearIndex' % (dataset),
-             '%s/ni/graph.html' % (outputDir))
-  cnt = Counter(aggni)
-  kc = (cnt.most_common(1))[0][0]
-  chosenBs = list(
-               map(lambda x: betas[x],
-                   filter(lambda y: aggni[y] == kc, range(10))))
-  print('@@@ k_C = %d, β chosen = %r' % (kc, chosenBs[len(chosenBs) // 2]))
-  nucleus = nx.k_core(G, kc)
-  print('N(GC) = %d, E(GC) = %d' %(nucleus.number_of_nodes(), nucleus.number_of_edges()))
-
+# ===================================================
+# ================== Main Function ==================
+# ===================================================
 
 def main():
-  if not (os.path.exists(outputDir)):
-    os.makedirs(outputDir)
-  if not (os.path.exists('%s/dep' % outputDir)):
-    os.makedirs('%s/dep' % outputDir)
-  if not (os.path.exists('%s/ni' % outputDir)):
-    os.makedirs('%s/ni' % outputDir)
+  if not (os.path.exists(OUTPUT_DIR)):
+    os.makedirs(OUTPUT_DIR)
+  if not (os.path.exists('%s/dep' % OUTPUT_DIR)):
+    os.makedirs('%s/dep' % OUTPUT_DIR)
+  if not (os.path.exists('%s/ni' % OUTPUT_DIR)):
+    os.makedirs('%s/ni' % OUTPUT_DIR)
+
   print('Reading edges...')
-  edges = readEdges(efile)
+  edges = readEdges(EDGES_FILE)
+
   G = nx.Graph()
-  print('Done!\nAdding edges...')
+
+  print('Adding edges...')
   G.add_edges_from(edges)
-  print('Done!\nCalculating core values after removing self-loops...')
+
+  print('Calculating core values after removing self-loops...')
   G.remove_edges_from(nx.selfloop_edges(G))
+
   print('# nodes = %d, # edges = %d' %(G.number_of_nodes(), G.number_of_edges()))
+
   cores = nx.core_number(G)
-  if corenum is not None:
-    queryg = nx.k_core(G, corenum)
-    print('kth core contains %d nodes and %d edges' % (queryg.number_of_nodes(), queryg.number_of_edges()))
-  coreFreq = Counter(cores.values())
-  cdf = []; s = 0
+
+  if core_num is not None:
+    query_subgraph = nx.k_core(G, core_num)
+    print('kth core contains %d nodes and %d edges' % (query_subgraph.number_of_nodes(), query_subgraph.number_of_edges()))
+
+  coreFreq = Counter(cores.values())        # coreFreq = {core_num: number_of_nodes}
+  coreDensityFunc = []; s = 0
   for x in range(max(coreFreq) - 1, -1, -1):
     s = s + coreFreq[x]
-    cdf.append(s)
-  print(list(reversed(cdf)))
-  dumpData(cores, '%s/cores.json' % (outputDir))
-  print('Done!\nGetting dependency values...',)
-  getDep(G, cores, outputDir)
+    coreDensityFunc.append(s)
+  print(list(reversed(coreDensityFunc)))
 
+  dumpData(cores, '%s/cores.json' % (OUTPUT_DIR))
+
+  print('Getting dependency values...',)
+  getDep(G, cores, OUTPUT_DIR)
+
+  # betweennessCentrality=nx.algorithms.centrality.betweenness_centrality(G)
+  # eigenvectorCentrality=nx.algorithms.centrality.eigenvector_centrality(G)
+  # katzCentrality=nx.algorithms.centrality.katz_centrality(G)
+  # closenessCentrality=nx.algorithms.centrality.closeness_centrality(G)
 
 if __name__ == '__main__':
   main()
